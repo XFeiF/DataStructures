175e50e782b3a19251253bb5eb70c38ee36b1ab4
XFeiF <eva.aeolus@gmail.com>
2016-09-24 01:46:58 +0800

    BF demo debug

--- 5-StringSearching/BF.cpp
+++ 5-StringSearching/BF.cpp
@@ -1,32 +1,6 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int index(const string &,const string &,int);
-
-int main()
-{
-	char ch;
-	do{
-		string Tag,Ptn;
-		int pos;
-		cout<<"输入主串：";
-		cin>>Tag;
-		cout<<"输入子串：";
-		cin>>Ptn;
-		cout<<"输入主串中开始进行匹配的位置（首字符位置为0）：";
-		cin>>pos;
-
-		int result = index(Tag,Ptn,pos);
-		if(result != -1)
-			cout<<"主串与子串在主串的第"<<result<<"个字符（首字符的位置为0）处首次匹配"<<endl;
-		else
-			cout<<"无匹配子串"<<endl;
-
-		cout<<"是否继续测试（输入y或Y继续，任意其他键结束）：";
-		cin>>ch;
-	}while(ch == 'y' || ch == 'Y');
-	return 0;
-}
 
 /*
 返回子串Ptn在主串Tag的第pos个字符后(含第pos个位置)第一次出现的位置，若不存在，则返回-1
---
+++ 5-StringSearching/BFDemo.cpp
@@ -0,0 +1,28 @@
+#include "BF.cpp"
+#include<string>
+using namespace std;
+
+int main()
+{
+	char ch;
+	do{
+		string Tag,Ptn;
+		int pos;
+		cout<<"输入主串：";
+		cin>>Tag;
+		cout<<"输入子串：";
+		cin>>Ptn;
+		cout<<"输入主串中开始进行匹配的位置（首字符位置为0）：";
+		cin>>pos;
+
+		int result = index(Tag,Ptn,pos);
+		if(result != -1)
+			cout<<"主串与子串在主串的第"<<result<<"个字符（首字符的位置为0）处首次匹配"<<endl;
+		else
+			cout<<"无匹配子串"<<endl;
+
+		cout<<"是否继续测试（输入y或Y继续，任意其他键结束）：";
+		cin>>ch;
+	}while(ch == 'y' || ch == 'Y');
+	return 0;
+}
