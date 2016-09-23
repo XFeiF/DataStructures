59c30423ff9ae8a628eccd1ef2b042fb5b09434b
XFeiF <eva.aeolus@gmail.com>
2016-09-24 01:47:33 +0800

    KMP demo and debug

--- 5-StringSearching/KMP_index.cpp
+++ 5-StringSearching/KMP_index.cpp
@@ -1,35 +1,11 @@
 #include<iostream>
 #include<string>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
 int kmp_index(const string &,const string &,int);
 void get_next(const string &,int *,int);
 void get_nextval(const string &,int *,int);
-
-int main()
-{
-	char ch;
-	do{
-		string Tag,Ptn;
-		int pos;
-		cout<<"输入主串：";
-		cin>>Tag;
-		cout<<"输入子串：";
-		cin>>Ptn;
-		cout<<"输入主串中开始进行匹配的位置（首字符位置为0）：";
-		cin>>pos;
-
-		int result = kmp_index(Tag,Ptn,pos);
-		if(result != -1)
-			cout<<"主串与子串在主串的第"<<result<<"个字符（首字符的位置为0）处首次匹配"<<endl;
-		else
-			cout<<"无匹配子串"<<endl;
-
-		cout<<"是否继续测试（输入y或Y继续，任意其他键结束）：";
-		cin>>ch;
-	}while(ch == 'y' || ch == 'Y');
-	return 0;
-}
-
 /*
 返回子串Ptn在主串Tag的第pos个字符后(含第pos个位置)第一次出现的位置，若不存在，则返回-1
 采用KMP算法，这里的位置全部以从0开始计算为准，其中T非空，0<=pos<=Tlen
