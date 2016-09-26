da09946c6e24803a6c8258b22757d49c433c0e5f
XFeiF <eva.aeolus@gmail.com>
2016-09-26 20:23:30 +0800

    Shortest Path debug

--- 8-Graph/AdjList/SP-Dijkstra.cpp
+++ 8-Graph/AdjList/SP-Dijkstra.cpp
@@ -1,59 +1,75 @@
 /*
 单源最短路径
 Dijkstra算法
+
 */
 
-#include "Graph.h"
-#include "log.h"
+//#include "Graph.h"
+//#include "log.h"
 #include <iostream>
 #include <cstdio>
 #include <queue>
 
 using namespace std;
-bool operator<(Node a, Node b){
-        return a->distance > b->distance;
-}
+
 
 void Dijkstra(PGraph &pG, graph_info info){
     int nodes = getNodeCount(pG);
+    int distance[nodes];                   //最短距离集
+	bool visited[nodes];                   //判断该点是否已经存入到S集合中
+    int arc[nodes][nodes];                 //邻接矩阵
+    int prev[nodes];                       //记录前驱节点
+
+     for(int i = 0; i < nodes; i++)
+     {
+         for(int j = 0; j < nodes; j++)
+            arc[i][j]= INT_MAX;
+        arc[i][i] = 0;
+     }
     
-    int *distance;
-	bool *visited;
-    PNode *pN;
-    distance = new int[nodes];
-    visited = new bool[nodes];
-    pN = new PNode[nodes];
-
-    priority_queue<Node> pQ;
-
-    for(int i = 0; i < nodes; i++){
-        visited[i] = false;
-        distance[i] = INT_MAX;
-        pN[i] = getNode(pG,i);
-    }
-
-    distance[info.source] = 0;
-    pQ.push(pN[info.source]);
-    while(!pQ.empty()){
-        PNode topN = pQ.top();
-        pQ.pop();
-        if(visited[topN->source])
-            continue;
-        visited[topN->source] = true;
-        
-        while(topN!=NULL){
-            if(!visited[topN->dest] &&
-             distance[topN->dest] > (topN->distance + distance[topN->distance])){
-                 visited[topN->dest] = true;
-                 distance[topN->dest] = topN->distance + distance[topN->distance];
-                 pQ.push(pN[topN->dest]);
-             }
-             topN = topN->next;
+    for (int i = 0; i < nodes; ++i) {
+        PNode n = getNode(pG,i);
+        while(n){
+          //  cout <<"source: "  << n->source << "  dest:" <<  n->dest << "  distance" << n->distance<<endl;
+            arc[n->source][n->dest] = n->distance;
+            n = n->next;
         }
     }
 
+    for(int i = 0; i < nodes; i++){
+        distance[i] = arc[info.source][i];
+        visited[i] = false;
+        // if(distance[i] == INT_MAX)
+        //     prev[i] = -1;
+        // else
+        //     prev[i] = info.source;
+    }
+    
+    visited[info.source] = true;
+    for(int i = 1; i < nodes; i++){
+        int mindist = INT_MAX;
+        int u =info.source;                     // 找出当前未使用的点j的distance[j]最小值
+        for(int j = 0; j < nodes; j++){
+            if(!visited[j] && distance[j] < mindist){
+                u = j;                           // u保存当前邻接点中距离最小的点的号码
+                mindist = distance[j];
+            }
+        }
+        visited[u] = true;
+        for(int j = 0; j < nodes; j++){
+            if(!visited[j] && arc[u][j] < INT_MAX){
+                if(distance[u] + arc[u][j] < distance[j]){
+                    distance[j] = distance[u] + arc[u][j];
+                    //prev[j] = u;                    
+                }
+            }
+        }
+    }
+       
+
+    cout << "The shortest path from Source Node " << info.source << " to other nodes are:" <<endl;
     for (int i = 0; i < nodes; ++i) {
-		LOG(distance[i]);
+		cout << i << ":  " << distance[i] << endl;
 	}
 
 }
--- 8-Graph/AdjList/SP-Floyd.cpp
+++ 8-Graph/AdjList/SP-Floyd.cpp
@@ -6,34 +6,45 @@ between every pair of vertices v, v' in the graph.
 Shortest path between all pairs of vertices, negative edges allowed.
 */
 
-#include "Graph.h"
+//#include "Graph.h"
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 void Floyd(PGraph pG){
     int nodes = getNodeCount(pG);
-    int distM[nodes][nodes];
-    for(int i = 0; i < nodes; i++)
-        for(int j = 0; j < nodes; j++){
+    int distM[nodes][nodes];         //邻接矩阵
+    //初始化  得到邻接矩阵   即距离矩阵
+    for(int i = 0; i < nodes; i++){
+        for(int j = 0; j < nodes; j++)
             distM[i][j] = INT_MAX;
-        }
-    
-    for(int i = 0; i < nodes; i++) distM[i][i] = 0;
-    
+        distM[i][i] = 0;
+    }
     for(int i = 0; i < nodes; i++){
         PNode it = getNode(pG,i);
-        while(it!=NULL){
+        while(it){
             distM[it->source][it->dest] = it->distance;
             it = it->next;
         }
     }
 
+    // for(int i = 0; i < nodes; i++){
+    //     for(int j = 0; j < <nodes; j++)
+    //         cout << distM[i][j] << "  ";
+    //     cout << endl;
+    // }
+
+
+
     int i,j,k;
     for(k = 0; k < nodes; k++)
-        for(i = 0; i < ndoes; i++)
-            for(j = 0; j < nodes; j++)
-                distM[i][j] =min(distM[i][j], distM[i][k] + distM[k][j]);
+        for(i = 0; i < nodes; i++)
+            for(j = 0; j < nodes; j++){
+                if(distM[i][j] - distM[i][k] > distM[k][j])   //这里千万注意 distM[i][j] > distM[i][k] + distM[k][j]
+                                                                //右边有可能溢出，所以变成减法
+                distM[i][j] = distM[i][k] + distM[k][j];
+               // cout << "debug in  " << distM[i][j] <<endl;
+            }
 
     for(i = 0; i < nodes; i++)
         for(j = 0;j < nodes; j++)
